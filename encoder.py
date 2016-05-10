8f0a33886a47ec7e17194adf6b7dfb1c8e59d650
Michele Pratusevich <mprat@alum.mit.edu>
2016-05-09 21:05:31 -0400

    Print encoder, not sorted

--- encoder.py
+++ encoder.py
@@ -18,12 +18,21 @@ def construct_dictionary(keyword):
 def encode(sentence, keyword):
 	encoder_dict = construct_dictionary(keyword)
 
-	return "".join(map(lambda x: encoder_dict.get(x, x), sentence))
+	return ("".join(map(lambda x: encoder_dict.get(x, x), sentence)), encoder_dict)
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description="Encode a sentnce with a keyword.")
 	parser.add_argument("-s", dest="sentence", type=str, required=True)
 	parser.add_argument("-k", dest="keyword", type=str, required=True)
+	parser.add_argument("--show_dict", required=False, action="store_true")
 
 	args = parser.parse_args()
-	print encode(args.sentence, args.keyword)
+	sentence = args.sentence
+	keyword = args.keyword
+	show_dict = args.show_dict
+	(encoded_sentence, encoder_dict) = encode(sentence, keyword)
+	print "Original sentence: %s" % sentence
+	print "Keyword: %s" % keyword
+	print "Encoded sentence: %s" % encoded_sentence
+	if show_dict:
+		print "Dictionary: %s" % encoder_dict
